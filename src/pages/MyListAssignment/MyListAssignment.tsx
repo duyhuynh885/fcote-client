b945f304b9c711bf2e5339d6b033b77013f15af8
huy <nguyentanhuy2711@gmail.com>
2022-06-17 00:13:18 +0700

    update

--- src/pages/MyListAssignment/MyListAssignment.tsx
+++ src/pages/MyListAssignment/MyListAssignment.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { Grid, Pagination, PaginationItem, Stack } from '@mui/material'
 import AssignmentItem from '../../components/Assignment/AssignmentItem/AssignmentItem'
 // import ButtonBack from "../../components/Button/ButtonBack/ButtonBack";
 import MyListAssignmentStyle from './style'
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 export default function MyListAssignment() {
   const classes = MyListAssignmentStyle()
   return (
@@ -62,6 +63,27 @@ export default function MyListAssignment() {
         <Grid className={classes.itemGrid} item xs={12} sm={6} md={4} lg={3}>
           <AssignmentItem />
         </Grid>
+        <Stack
+          style={{
+            marginLeft: '34%',
+            marginTop: '20px',
+            marginBottom: '20px',
+          }}
+          spacing={2}
+        >
+          <Pagination
+            count={10}
+            renderItem={item => (
+              <PaginationItem
+                components={{
+                  previous: ArrowBackIcon,
+                  next: ArrowForwardIcon,
+                }}
+                {...item}
+              />
+            )}
+          />
+        </Stack>
       </Grid>
     </div>
   )
